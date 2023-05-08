72201e98ac1a26dfa1fb9308e3aed8cb5fd39530
Luke-Angelo Strazzera <98197481+LukeStrazz@users.noreply.github.com>
2023-05-08 19:48:39 -0400

    Happy Working LinkedList

--- src/main/java/p1/TextGenerator.java
+++ src/main/java/p1/TextGenerator.java
@@ -5,10 +5,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.File;
@@ -35,11 +32,12 @@ public class TextGenerator extends Application {
     public void start(Stage primaryStage) {
         this.stage = primaryStage;
         Pane loginGui = welcome();
-        welcomeScene = new Scene(loginGui, 700, 600);
-
+        welcomeScene = new Scene(loginGui, 500, 450);
+        welcomeScene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
         primaryStage.setScene(welcomeScene);
         primaryStage.show();
     }
+
 
     private void processFile(Path filePath) throws IOException {
         Pattern wordPattern = Pattern.compile("\\b\\w+\\b");
@@ -69,12 +67,12 @@ public class TextGenerator extends Application {
     }
 
     private Pane welcome() {
-        Pane welcome = new Pane();
+        Pane welcome = new StackPane(); // Use StackPane instead of Pane
         // Welcome Screen
         VBox welcomeScreen = new VBox(20);
         welcomeScreen.setAlignment(Pos.CENTER);
         welcomeScreen.setPadding(new Insets(10));
-        Label welcomeLabel = new Label("Welcome to Text Generator");
+        Label welcomeLabel = new Label("Welcome to Synonym Swing");
         welcomeLabel.setStyle("-fx-font-size: 20;");
         Button returningUserButton = new Button("Returning user");
         Button firstTimeUserButton = new Button("First-time user");
@@ -82,24 +80,27 @@ public class TextGenerator extends Application {
 
         // Button actions
         returningUserButton.setOnAction(e -> {
-            appScene = new Scene(buildApp(), 700, 600);
-            //appScene.getStylesheets().add(getClass().getResource("ApplicationStyle.css").toExternalForm());
+            appScene = new Scene(buildApp(), 500, 450);
+            appScene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
             stage.setScene(appScene);
         });
 
         firstTimeUserButton.setOnAction(e -> {
-            instructionScene = new Scene(instructions(), 700, 600);
-            //instructionScene.getStylesheets().add(getClass().getResource("ApplicationStyle.css").toExternalForm());
+            instructionScene = new Scene(instructions(), 500, 450);
+            instructionScene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
             stage.setScene(instructionScene);
         });
 
+        // Add VBox to the StackPane and center it
         welcome.getChildren().add(welcomeScreen);
+        StackPane.setAlignment(welcomeScreen, Pos.CENTER);
+
         return welcome;
     }
 
+
     private Pane instructions() {
         BorderPane instructions = new BorderPane();
-
         // Instructions
         VBox instructionBox = new VBox(10);
         instructionBox.setAlignment(Pos.CENTER);
@@ -108,14 +109,14 @@ public class TextGenerator extends Application {
         Label step1 = new Label("1. Click 'File' in the menu bar to select a text file.");
         Label step2 = new Label("2. Enter a starting word and a word count.");
         Label step3 = new Label("3. Click 'Generate' to generate new text.");
-                Button proceedButton = new Button("Proceed");
+        Button proceedButton = new Button("Proceed");
         instructionBox.getChildren().addAll(instructionLabel, step1, step2, step3, proceedButton);
 
 
         proceedButton.setOnAction(e -> {
             instructions.setVisible(false);
-            appScene = new Scene(buildApp(), 700, 600);
-            //appScene.getStylesheets().add(getClass().getResource("ApplicationStyle.css").toExternalForm());
+            appScene = new Scene(buildApp(), 500, 450);
+            appScene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
             stage.setScene(appScene);
         });
 
@@ -144,7 +145,7 @@ public class TextGenerator extends Application {
         inputPanel.getChildren().add(startingWordField);
         inputPanel.getChildren().add(new Label("Word count:"));
         wordCountField = new TextField();
-        wordCountField.setPrefWidth(50);
+        wordCountField.setPrefWidth(69);
         wordCountField.setDisable(true);
         inputPanel.getChildren().add(wordCountField);
         Button generateButton = new Button("Generate");
@@ -152,19 +153,17 @@ public class TextGenerator extends Application {
         inputPanel.getChildren().add(generateButton);
 
         textArea = new TextArea();
-        textArea.setPrefWidth(700);
-        textArea.setPrefHeight(500);
+        textArea.setPrefWidth(500);
+        textArea.setPrefHeight(369);
         textArea.setDisable(false); //scroll or not
         textArea.setWrapText(true);
-        ScrollPane scrollPane = new ScrollPane(textArea);
 
-        VBox root = new VBox(menuBar, inputPanel, scrollPane);
+        VBox root = new VBox(menuBar, inputPanel, textArea);
 
         BorderPane app = new BorderPane();
         app.setTop(menuBar);
         app.setCenter(inputPanel);
-        app.setBottom(scrollPane);
-
+        app.setBottom(textArea);
         Path readMEPath = Paths.get("src/main/java/p1/data/readME2.txt");
 
         try {
@@ -230,7 +229,6 @@ public class TextGenerator extends Application {
                 System.err.println("Error reading file: " + ex.getMessage());
             }
         });
-
 
         generateButton.setOnAction(e -> {
             Boolean good2go = false;
